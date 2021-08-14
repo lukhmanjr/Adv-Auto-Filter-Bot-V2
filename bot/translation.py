afc8b3f34032b58b123ed849c6b3bb2a10d4d2da
lukhmanjr <85250684+lukhmanjr@users.noreply.github.com>
2021-08-14 13:06:12 +0530

    Update translation.py

--- bot/translation.py
+++ bot/translation.py
@@ -48,11 +48,11 @@ For More Details Click Help Button Below..
     
     ABOUT_TEXT = """<b>➥ Name</b> : <code> Auto Filter Bot</code>
     
-<b>➥ Creator</b> : <b><i><a href="https://t.me/AlbertEinstein_TG">@lukhman_jr</a></i></b>
+<b>➥ Creator</b> : <b><i><a href="https://t.me/lukhman_jr">@lukhman_jr</a></i></b>
 
 <b>➥ Language</b> : <code>Python3</code>
 
 <b>➥ Library</b> : <i><a href="https://docs.pyrogram.org">Pyrogram Asyncio 1.13.0 </a></i>
 
-<b>➥ Source Code</b> : <i><a href="https://github.com/AlbertEinsteinTG/Adv-Auto-Filter-Bot">Click Me</a></i>
+<b>➥ Source Code</b> : <i><a href="">Click Me</a></i>
 """
