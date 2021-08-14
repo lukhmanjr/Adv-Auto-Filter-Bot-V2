6590b9e762bafc7a413856e9fa0ad2e206ec85d7
lukhmanjr <85250684+lukhmanjr@users.noreply.github.com>
2021-08-14 13:08:18 +0530

    Update commands.py

--- bot/plugins/commands.py
+++ bot/plugins/commands.py
@@ -57,7 +57,7 @@ async def start(bot, update):
                         [
                             InlineKeyboardButton
                                 (
-                                    'Developers', url="https://t.me/CrazyBotsz"
+                                    'Developers', url="https://t.me/lukhman_jr"
                                 )
                         ]
                     ]
